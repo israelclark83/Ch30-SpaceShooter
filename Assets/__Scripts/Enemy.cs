68f1cf82a00aa8bf0aef086381bc16a70f9e9d82
israelclark83 <54966452+israelclark83@users.noreply.github.com>
2019-11-07 15:50:38 -0500

    Updated Enemy and Bounds scripts

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;      // The speed in m/s
+    public float fireRate = 0.3f;  // Seconds/shot (Unused)
+    public float health = 10;
+    public int score = 100;      // Points earned for destroying this
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
     {
-        
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // This is a Property: A method that acts like a field
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        if (bndCheck != null && bndCheck.offDown)
+        {
+            // We're off the bottom, so destroy this GameObject
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
     }
 }
